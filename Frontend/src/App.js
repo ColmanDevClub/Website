95f9b74812ad8729cf0998dbb325659f76ea4ee6
Idan Asayag <idanasayag0@gmail.com>
2023-09-03 18:33:18 +0300

    add footer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,21 +6,28 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import Playground from "./Playground";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
+import Footer from "./components/Footer";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Navbar />}>
-      <Route index element={<HomePage />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Route>
+    <>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Route>
+    </>
   )
 );
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ Frontend/src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export default function DenseAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: "#222222" }}>
+        <Toolbar variant="dense">
+          <Typography variant="p" color="inherit" component="div">
+            ColmanDevClub
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
