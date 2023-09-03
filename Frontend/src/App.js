2758c0c7594862aac61b26149ca419e8e3e9c870
Idan Asayag <idanasayag0@gmail.com>
2023-09-03 17:32:01 +0300

    add playground

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Playground from "./Playground";
 const App = () => {
-  return <Navbar />;
+  return <Playground />;
 };
 
 export default App;
