29b97beb59927ff5e8979757649ce3259e7a1b16
Idan Asayag <idanasayag0@gmail.com>
2023-09-06 18:52:59 +0300

    make icon generic

--- Frontend/src/components/Footer/index.js
+++ Frontend/src/components/Footer/index.js
@@ -1,51 +1,25 @@
 import * as React from "react";
 
 import { Container, AppBar, Box, Toolbar, Typography } from "@mui/material";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import CopyrightIcon from "@mui/icons-material/Copyright";
+import { GitHub, LinkedIn } from "@mui/icons-material";
 
 import css from "./style.module.css";
 
 export default function DenseAppBar() {
+  const iconStyle = { fontSize: { xs: "1.5rem", lg: "1.75rem" } };
+  const iconsLink = [
+    {
+      href: "https://github.com/ColmanDevClub",
+      icon: <GitHub sx={iconStyle} />,
+    },
+    {
+      href: "https://www.linkedin.com/company/colman-devclub/",
+      icon: <LinkedIn sx={iconStyle} />,
+    },
+  ];
+
   return (
     <>
-      {/* <Box display={"flex"} alignItems="center" style={{ display: "none" }} sx={{ display: {xs: 'none', md: "none !important" } }}>
-        <AppBar position="static" sx={{ backgroundColor: "#222222", padding: '0.5rem' }}>
-          <Container maxWidth="xl">
-            <Toolbar variant="dense" sx={{display: 'flex', justifyContent: 'space-between'}}>
-              <Typography variant="h6" noWrap component="a" href="/" sx={{
-                  mr: 2,
-                  display: { xs: "flex", md: "flex" },
-                  fontWeight: 700,
-                  color: "inherit",
-                  textDecoration: "none",
-                }}
-              >
-                Colman<span className={css["text-yellow"]}>Dev</span>Club
-              </Typography>
-              <Box sx={{ display: "flex" }} gap="1rem">
-                <GitHubIcon />
-                <InstagramIcon />
-                <LinkedInIcon />
-              </Box>
-              <Typography variant="p" sx={{
-                marginX: '0',
-                marginY: '0',
-                color: '#999999',
-                display: 'flex',
-                alignItems: 'center',
-                gap: '0.5rem'
-              }}>
-                <CopyrightIcon />
-                All Rights Reserved To ColmanDevClub
-              </Typography>
-            </Toolbar>
-          </Container>
-        </AppBar>
-      </Box> */}
-
       <Box alignItems="center" sx={{ display: { xs: "block", md: "block" } }}>
         <AppBar
           position="static"
@@ -76,36 +50,19 @@ export default function DenseAppBar() {
                 Colman<span className={css["text-yellow"]}>Dev</span>Club
               </Typography>
               <Box sx={{ display: "flex" }} gap="0.75rem">
-                <a
-                  href="https://github.com/ColmanDevClub"
-                  className={css["icon"]}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <GitHubIcon
-                    sx={{ fontSize: { xs: "1.5rem", lg: "1.75rem" } }}
-                  />
-                </a>
-                <a
-                  href="/"
-                  className={css["icon"]}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <InstagramIcon
-                    sx={{ fontSize: { xs: "1.5rem", lg: "1.75rem" } }}
-                  />
-                </a>
-                <a
-                  href="https://www.linkedin.com/company/colman-devclub/"
-                  className={css["icon"]}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <LinkedInIcon
-                    sx={{ fontSize: { xs: "1.5rem", lg: "1.75rem" } }}
-                  />
-                </a>
+                {iconsLink.map((icon) => {
+                  return (
+                    <a
+                      key={icon.href}
+                      href={icon.href}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={css["icon"]}
+                    >
+                      {icon.icon}
+                    </a>
+                  );
+                })}
               </Box>
               <Typography
                 component="p"
