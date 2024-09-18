e9df2f34d45d46c2f968b21c7ef23884aed05f4e
GilHeller <gil151200@gmail.com>
2024-09-18 20:44:19 +0300

    CR: pass width & height from props

--- src/components/YouTubeVideo.jsx
+++ src/components/YouTubeVideo.jsx
@@ -1,15 +1,15 @@
 import { Box } from '@mui/material';
 import React from 'react';
 
-export const YouTubeVideo = ({ videoId }) => {
+export const YouTubeVideo = ({ videoId, width, height }) => {
   return (
     <Box display={"flex"} justifyContent={"center"}>
       <iframe 
         id={`ytplayer-${videoId}`} 
         type="text/html" 
         title="YouTube video player"
-        width={"100%"}
-        height={"300px"}
+        width={width}
+        height={height}
         src={`https://www.youtube.com/embed/${videoId}`}
         frameBorder="0" 
         allowFullScreen
