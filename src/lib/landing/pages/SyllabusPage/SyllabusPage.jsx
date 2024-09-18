d2cf052a74a8dacd9b81beab9e88eb493a703088
GilHeller <gil151200@gmail.com>
2024-09-18 20:48:49 +0300

    CR: extract parsing functions to helpers.js

--- src/lib/landing/pages/SyllabusPage/SyllabusPage.jsx
+++ src/lib/landing/pages/SyllabusPage/SyllabusPage.jsx
@@ -3,27 +3,8 @@ import React from 'react';
 import Loader from '../../../../ui/Loader';
 import useGoogleSheetsData from '../../../../hooks/useSheets';
 import { SyllabusCard } from './SyllabusCard';
+import { syllabusCsvDataParser } from './helpers';
 
-
-const strip = (str) => {
-  return str?.replace(/^\s+|\s+$|"|'/g, '');
-};
-
-const getVideoId = (youtubeUrl) => {
-  return youtubeUrl?.split('v=')[1]?.split('&')[0]
-}
-
-const csvMap = {
-  id: 0,
-  subject: 1,
-  presentation: 2,
-  youtube: 3,
-  links: 4,
-  git: 5,
-  exercise: 6,
-  time: 7,
-  show: 8,
-};
 
 const SyllabusPage = () => {
   const { data, isLoading, error } = useGoogleSheetsData();
@@ -45,24 +26,7 @@ const SyllabusPage = () => {
     );
   }
 
-  const splicedData = data?.slice(1, data?.length);
-  const visibleData = splicedData?.filter((row) => strip(row[csvMap.show]) === 'TRUE');
-  const csvData = visibleData?.map((row) => {
-
-    const rawData = strip(row[csvMap.youtube]);    
-    const videosList = rawData.split(',');
-    const videosIds = videosList?.map(videoUrl => getVideoId(videoUrl));
-    
-    return {
-      id: strip(row[csvMap.id]),
-      subject: strip(row[csvMap.subject]),
-      presentation: strip(row[csvMap.presentation]),
-      youtube: videosIds,
-      git: strip(row[csvMap.git]),
-      exercise: strip(row[csvMap.exercise]),
-      time: strip(row[csvMap.time]),
-    };
-  });
+  const csvData = syllabusCsvDataParser(data);
 
   return (
     <Container display="flex"  width="100%">
